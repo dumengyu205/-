64e788dac7756f3dd27274888b61a8f5c97c1181
dumengyu205 <55050730+dumengyu205@users.noreply.github.com>
2019-11-03 14:15:00 +0800

    在类中使用一个数组来表示存储空间

---
+++ Example015/src/Storage.java
@@ -0,0 +1,18 @@
+class Storage{
+	private int[] cells=new int[10];
+	private int inPos,outPos;
+	public void put (int num) {
+		cells[inPos]=num;
+		System.out.println("在cells["+inPos+"]中放入数据---"+cells[inPos]);
+		inPos++;
+		if(inPos==cells.length)
+			inPos=0;
+	}
+public void get() {
+	int data=cells[outPos];
+	System.out.println("从cells["+outPos+"]中取出数据"+data);
+	outPos++;
+	if(outPos==cells.length)
+		outPos=0;
+    }
+}
