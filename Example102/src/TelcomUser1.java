4abf13c6673704850003c3da5ec0342a61203d0e
dumengyu205 <55050730+dumengyu205@users.noreply.github.com>
2019-11-24 14:42:02 +0800

    Enumeration，for each，ListIterator，Iterator，arrayListSize

---
+++ Example102/src/TelcomUser1.java
@@ -0,0 +1,78 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+class TelcomUser1 {
+	private String phoneNumber;
+	private String callTo;
+	private Vector communicationRecords;
+	private TreeSet callToNumbersSet;
+	private ArrayList callToNumbersList;
+	public TelcomUser1(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		this.communicationRecords = new Vector();
+		this.callToNumbersSet = new TreeSet();
+		this.callToNumbersList = new ArrayList();
+	}
+	
+	void generateCommunicateRecord() {
+		int recordNum = new Random().nextInt(10);
+		for(int i = 0; i <= recordNum; i++) {
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.HOUR, - new Random().nextInt(10));
+			long timeStart = cal.getTimeInMillis();
+			long timeEnd = timeStart + 60000 + new Random().nextInt(600000);			
+			this.callTo = getCallToPhoneNumber();
+			this.callToNumbersList.add(this.callTo);
+			this.callToNumbersSet.add(this.callTo);
+			this.communicationRecords.add(this.phoneNumber + 
+					"," + timeStart + 
+					"," + timeEnd + 
+					"," + this.callTo);
+		}
+	}
+	
+	private String getCallToPhoneNumber() {
+		return "1380372" + String.valueOf(new Random().nextInt(10))
+			 + String.valueOf(new Random().nextInt(10))
+			 + String.valueOf(new Random().nextInt(10))
+			 + String.valueOf(new Random().nextInt(10));
+
+	}
+	
+	private String accountFee(long timeStart, long timeEnd) {
+		double feePerMinute = 0.2;
+		int minutes = Math.round((timeEnd - timeStart)/60000);
+		double feeTotal = feePerMinute * minutes;
+		return String.format("%.4f", feeTotal);
+	}
+	
+	void printCallToNumber() {
+		for(Object callTo:this.callToNumbersList) {
+			System.out.println((String)callTo);
+		}
+		System.out.println("--------List/Set分割线---------");
+		for(Object callTo:this.callToNumbersSet) {
+			System.out.println((String)callTo);
+		}
+	}
+	
+	void printDetails() {				
+		for(Object aRecord:this.communicationRecords) {
+			System.out.println("---------通话记录分割线---------");
+			String [] recordField = ((String)aRecord).split(",");
+			System.out.println("主叫：" + recordField[0]);
+			System.out.println("被叫：" + recordField[3]);
+			Date timeStart = new Date(Long.parseLong(recordField[1]));
+			Date timeEnd = new Date(Long.parseLong(recordField[2]));			
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年MM月dd日 hh时mm分ss秒");
+			
+			//SimpleDateFormat			
+			System.out.println("通话开始时间：" + simpleDateFormat.format(timeStart));
+			System.out.println("通话结束时间：" + simpleDateFormat.format(timeEnd));
+			
+			System.out.println("计费：" 
+					+ accountFee(Long.parseLong(recordField[1]), Long.parseLong(recordField[2]))
+					+ " 元。");
+		}
+	}
+}
