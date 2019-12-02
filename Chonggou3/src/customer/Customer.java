9cdfcf65bf48bbec3dbbd91acbf3091993dbc942
dumengyu205 <55050730+dumengyu205@users.noreply.github.com>
2019-12-02 22:33:48 +0800

    重构  搬移“金额计算”代码  （影片出租）

---
+++ Chonggou3/src/customer/Customer.java
@@ -0,0 +1,53 @@
+package customer;
+import java.util.Enumeration;
+import java.util.Vector;
+import movie.*;
+import rental.*;
+
+public class Customer {
+	private String _name;
+	private Vector _rentals = new Vector();
+	
+	public Customer (String name){
+		_name = name;
+	};
+	
+	public void addRental(Rental arg){
+		_rentals.addElement(arg);
+	}
+	public String getName(){
+		return _name;
+	};
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String result ="Rental Record for" + getName() + "\n";
+		while (rentals.hasMoreElements()) {
+			double thisAmount = 0;
+			Rental each = (Rental) rentals.nextElement();
+			
+			thisAmount = each.getCharge();
+			
+			//add frequent renter points
+			frequentRenterPoints ++;
+			//add bonus for a two day new release rental
+			if((each.getMovie().getPrinceCode() == Movie.NEW_RELEASE)&&
+				each.getDaysRented() > 1) frequentRenterPoints ++;
+			
+			//show figures for this rental
+			result += "\t" + each.getMovie().getTitle()+ "\t" +
+			      String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
+			
+		}
+		//add footer lines
+		result +="Amount owed is" + String.valueOf(totalAmount) + "\n";
+		result +="You earned" + String.valueOf(frequentRenterPoints)+
+				"frequent renter points";
+		return result;
+	}
+private double amountFor(Rental aRental){
+	return aRental.getCharge();
+}
+}
