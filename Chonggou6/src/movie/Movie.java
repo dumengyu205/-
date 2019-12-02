1019dc2c1ea1ef186e6a22097c63064ab630bba3
dumengyu205 <55050730+dumengyu205@users.noreply.github.com>
2019-12-02 22:38:04 +0800

    重构   运用多态取代与价格相关的条件逻辑  （影片出租）

---
+++ Chonggou6/src/movie/Movie.java
@@ -0,0 +1,54 @@
+package movie;
+
+public class Movie {
+	public static final int CHILDRENS = 2;
+	public static final int REGULAR = 0;
+	public static final int NEW_RELEASE = 1;
+
+	private String _title;
+	private int _priceCode;
+	
+	public Movie(String title,int priceCode) {
+		_title = title;
+		_priceCode = priceCode;
+	}
+	
+	public int getPrinceCode() {
+		return _priceCode;
+	}
+	
+	public void setPriceCode(int arg){
+		_priceCode = arg;
+	}
+	
+	public String getTitle (){
+		return _title;
+	};
+	public double getCharge(int daysRented){
+		double result = 0;
+		switch(getPrinceCode()){
+		   case Movie.REGULAR:
+		        result += 2;
+		        if (daysRented >2)
+			        result += (daysRented - 2) *1.5;
+		        break;
+		   case Movie.NEW_RELEASE:
+			   result +=daysRented * 3;
+			   break;
+		   case Movie.CHILDRENS:
+		        result += 1.5;
+		        if(daysRented > 3)
+		        	result += (daysRented - 3) * 1.5;
+		        break;
+		}
+		return result;
+	}
+
+	public int getFrequenterPoints(int daysRented) {
+		if((getPrinceCode()==Movie.NEW_RELEASE)
+				&& daysRented>1)
+			return 2;
+		else 
+			return 1;
+	}
+}
